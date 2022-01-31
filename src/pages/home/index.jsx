b6ef90cc21cab57dbc1781908c2bacc74d221a69
Basil Babu <basilb@newagesmb.com>
2022-01-31 11:53:46 +0530

    pagination

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,33 +1,48 @@
-import { Card, Col, Row, Skeleton } from "antd";
+import { Card, Col, Row, Skeleton, Pagination } from "antd";
 import { Content } from "antd/lib/layout/layout";
-import React from "react";
-import { useGetPostByLimitQuery } from "services/post";
+import React, { useState } from "react";
+import { useGetAllPostQuery } from "services/post";
 
 function Index() {
-  const responseInfo = useGetPostByLimitQuery(10);
+  const responseInfo = useGetAllPostQuery(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage,setPostsPerPage] = useState(10);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+  const handlePagination =(page, pageSize)=>{
+    setCurrentPage(page)
+    setPostsPerPage(pageSize)
+
+    
+  }
   if (responseInfo.isLoading)
     return (
       <Content>
         <Row>
-        {[1,2,3,4,5,6,7].map((post, i) => (
-          <Col span={6} key={i}>
-            <Card
-              hoverable
-              style={{ margin: 20, height: 250 }}
-              title={post.title}
-            >
-             <Skeleton/>
-            </Card>
-          </Col>
-        ))}</Row>
+          {[1, 2, 3, 4, 5, 6, 7].map((post, i) => (
+            <Col span={6} key={i}>
+              <Card
+                hoverable
+                style={{ margin: 20, height: 250 }}
+                title={post.title}
+              >
+                <Skeleton />
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Content>
     );
   if (responseInfo.isError)
     return <h1>An error occured {responseInfo.error.error}</h1>;
+    const currentPosts = responseInfo.data.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
     <Content style={{ margin: "24px 16px 0" }}>
+      <Pagination defaultCurrent={1} onChange={handlePagination} total={responseInfo?.data.length} />
       <Row>
-        {responseInfo?.data.map((post, i) => (
+        {responseInfo?.data && currentPosts.map((post, i) => (
           <Col span={6} key={i}>
             <Card
               hoverable
