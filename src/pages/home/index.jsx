9a2fd7feee4a7d03b50d1f040d029651ada32d39
Basil Babu <basilb@newagesmb.com>
2022-01-31 12:51:16 +0530

    url push

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,27 @@
 import { Card, Col, Row, Skeleton, Pagination } from "antd";
 import { Content } from "antd/lib/layout/layout";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useGetAllPostQuery } from "services/post";
 
 function Index() {
+  const navigate = useNavigate();
   const responseInfo = useGetAllPostQuery(10);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage,setPostsPerPage] = useState(10);
+  const [postsPerPage, setPostsPerPage] = useState(10);
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  let [searchParams] = useSearchParams();
+  let urlPage = searchParams.get("page");
+  useEffect(() => {
+    setCurrentPage(urlPage);
+  }, [urlPage]);
 
-  const handlePagination =(page, pageSize)=>{
-    setCurrentPage(page)
-    setPostsPerPage(pageSize)
-
-    
-  }
+  const handlePagination = (page, pageSize) => {
+    setCurrentPage(page);
+    setPostsPerPage(pageSize);
+    navigate("?page=" + page);
+  };
   if (responseInfo.isLoading)
     return (
       <Content>
@@ -36,23 +42,31 @@ function Index() {
     );
   if (responseInfo.isError)
     return <h1>An error occured {responseInfo.error.error}</h1>;
-    const currentPosts = responseInfo.data.slice(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = responseInfo.data.slice(
+    indexOfFirstPost,
+    indexOfLastPost
+  );
 
   return (
     <Content style={{ margin: "24px 16px 0" }}>
-      <Pagination defaultCurrent={1} onChange={handlePagination} total={responseInfo?.data.length} />
+      <Pagination
+        defaultCurrent={urlPage || 1}
+        onChange={handlePagination}
+        total={responseInfo?.data.length}
+      />
       <Row>
-        {responseInfo?.data && currentPosts.map((post, i) => (
-          <Col span={6} key={i}>
-            <Card
-              hoverable
-              style={{ margin: 20, height: 250 }}
-              title={post.title}
-            >
-              <p>{post.body}</p>
-            </Card>
-          </Col>
-        ))}
+        {responseInfo?.data &&
+          currentPosts.map((post, i) => (
+            <Col span={6} key={i}>
+              <Card
+                hoverable
+                style={{ margin: 20, height: 250 }}
+                title={post.title}
+              >
+                <p>{post.body}</p>
+              </Card>
+            </Col>
+          ))}
       </Row>
     </Content>
   );
